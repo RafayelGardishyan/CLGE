18cf3151a50dd55dd44f4a5f1bd556da582db53d
RafayelGardishyan <rgardishyan@gmail.com>
2018-02-10 17:46:46 +0100

    Added Example Tests for Snake

---
+++ SnakeTests.py
@@ -0,0 +1,65 @@
+from clge import Tester, Screen, generate_keymap
+
+t = Tester()
+
+
+class SnakeTestException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+def test_screen():
+    scr = Screen(20, 20, auto_clear_objects_list=True, timeout=.5, auto_timeout=False, default_color=39)
+    return scr
+
+
+def test_tail():
+    tailX = [None] * 20
+    tailY = [None] * 20
+    for j in range(5):
+        prevX = tailX[0]
+        prevY = tailY[0]
+        tailX[0] = j
+        tailY[0] = j*2
+        prev2X = 0
+        prev2Y = 0
+        for i in range(1, 20):
+            prev2X = tailX[i]
+            prev2Y = tailY[i]
+            tailX[i] = prevX
+            tailY[i] = prevY
+            prevX = prev2X
+            prevY = prev2Y
+
+def test_keyboard():
+    keys = generate_keymap({
+        'up': 'w',
+        'up_arrow': 'up',
+        'down': 's',
+        'down_arrow': 'down',
+        'right': 'd',
+        'right_arrow': 'right',
+        'left': 'a',
+        'left_arrow': 'left',
+    })
+    for key in keys:
+        t.simulate_keyboard_press(keys[key].char)
+        if keys[key].detect():
+            pass
+        else:
+            t.simulate_keyboard_release(keys[key].char)
+            raise SnakeTestException("Key Not Detected")
+        t.simulate_keyboard_release(keys[key].char)
+
+def test_collision(cases):
+    for i in cases:
+        if i[0] == i[1] and i[2] == i[3]:
+            print(i)
+        else:
+            pass
+
+
+t.Test(test_screen)
+t.Test(test_keyboard)
+t.Test(test_tail)
+t.Test(test_collision, [[0, 1, 2, 3], [0, 0, 2, 4], [3, 1, 3, 3],])
+t.printTestResults()
--- clge/testing/simple_test.py
+++ clge/testing/simple_test.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import coverage
+import keyboard
 
 class Testing:
+    testCount = 0
+    passedTestCount = 0
+    not_passed = []
     # Code Coverage is an expoerimental function
     def CoverageStart(self):
         print("INFO: Code Coverage is an experimental function. It does not work good.")
@@ -19,15 +23,18 @@ class Testing:
         print("\n")
 
     def Test(self, function, *args, **kwargs):
+        self.testCount += 1
         print("INFO: Testing: \"" + function.__name__ + "\"")
         self.blockPrint()
         try:
             function(*args, **kwargs)
             self.enablePrint()
             print("INFO: Testing \"" + function.__name__ + "\" passed\n\n")
+            self.passedTestCount += 1
         except Exception as e:
+            self.not_passed.append(function.__name__)
             self.enablePrint()
-            print("INFO: Testing \"" + function.__name__ +"\" is not passed\n")
+            print("WARNING: Test \"" + function.__name__ +"\" is not passed")
             print("ERROR: " + str(e) + "\n\n")
 
     def blockPrint(self):
@@ -35,3 +42,16 @@ class Testing:
 
     def enablePrint(self):
         sys.stdout = sys.__stdout__
+
+    def printTestResults(self):
+        percent = int(self.passedTestCount * 100 / self.testCount)
+        print("{} percent of test are passed ({} / {})".format(percent, self.passedTestCount, self.testCount))
+        print("Not passed:")
+        for test in self.not_passed:
+            print("{}: {}".format(self.not_passed.index(test) + 1, test))
+
+    def simulate_keyboard_press(self, char):
+        keyboard.press(char)
+
+    def simulate_keyboard_release(self, char):
+        keyboard.release(char)
