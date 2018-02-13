4c263e82c4a718dc91b72910ddf1b9bbb99ba454
RafayelGardishyan <rgardishyan@gmail.com>
2018-02-13 20:00:35 +0000

    Added sound optimization for linux

--- clge/audio_player.py
+++ clge/audio_player.py
@@ -1,4 +1,5 @@
 from .exceptions import CLGEException
+from .setup import get_platform
 try:
     from playsound import playsound
 except ImportError:
@@ -10,13 +11,17 @@ class AudioPlayer:
 
     def __init__(self, path_to_file, async=True):
         self.path_to_file = path_to_file
-        if async == True:
-            self.async = False
-        elif async == False:
+        if get_platform() == "Windows":    
+            if async == True:
+                self.async = False
+            elif async == False:
+                self.async = True
+        else:
             self.async = True
 
     def play(self):
-        playsound(self.path_to_file, self.async)
+        if get_platform() == "Windows":
+            playsound(self.path_to_file, self.async)
 
     def __str__(self):
         return "Audio Player Object"
