7a12a74fb97f194144d115b6e047fe727a069f04
RafayelGardishyan <rgardishyan@gmail.com>
2018-02-15 17:48:11 +0100

    Maked 'default assets' a built-in plugin

--- clge/__init__.py
+++ clge/__init__.py
@@ -5,7 +5,6 @@ from .key_detector import KeyDetector, convert_to_char, convert_to_code, generat
 from .mouse_detector import MouseDetector, generate_mousemap
 from .audio_player import AudioPlayer
 from .painter import paint_text
-from . import default_assets as DefaultAssets
 from .testing import Tester as SimpleTester
 from .exceptions import CLGEException
 from .project_creator import ProjectCreator
