0a895fe72124bb64b834554821d9bf20ecd21c20
Rafayel Gardishyan <rgardishyan@gmail.com>
2018-02-20 20:34:46 +0100

    Update audio_player.py

--- clge/audio_player.py
+++ clge/audio_player.py
@@ -2,9 +2,6 @@ from .setup import get_platform
 from playsound import playsound
 
 class AudioPlayer:
-    path_to_file = None
-    async = False
-
     def __init__(self, path_to_file, async=True):
         self.path_to_file = path_to_file
         if get_platform() == "Windows":
