6c079acc16abb0ab6f000a9b5a89fce497f968da
RafayelGardishyan <rgardishyan@gmail.com>
2018-08-17 21:36:29 +0100

    Some File structure rearrangements

--- clge/plugs/DefaultAssets/__init__.py
+++ clge/plugs/DefaultAssets/__init__.py
@@ -1,13 +1,7 @@
-"""
-CLGE Plugin: Default Assets (Built-in)
-Contains:
-    * Sample Object
-    * Sample Player
-    * Sample Polygon
-
-Author: Rafayel Gardishyan
-Last update year: 2018
-"""
-from .sample_player import SamplePlayer
-from .sample_object import SampleObject
+# Old Files :: Not deleted for future change
 from .sample_polygon import SamplePolygon
+from .sample_object import SampleObject
+from .sample_player import SamplePlayer
+
+# New Behaviour Object Logic
+from .Behaviour import *
