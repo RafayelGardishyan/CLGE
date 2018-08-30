1c2d894051d4dbea1b4f1c46c9848126b2483fb0
RafayelGardishyan <rgardishyan@gmail.com>
2018-08-30 13:13:41 +0100

    Animated splash screen

--- clge/splash_screen.py
+++ clge/splash_screen.py
@@ -2,21 +2,25 @@ import time
 
 from .drawer import Screen
 
+offset = 4
+
 
 scr = Screen(20, 10, False, "*")
 
-scr.add_polygon(12, 5, 4, 1, u'\u2593')
-scr.add_polygon(10, 5, 5, 1, u'\u2591')
-scr.add_polygon(8, 3, 6, 2, u'\u2592')
+scr.auto_clear_objects_list_setter(True)
 
-scr.add_string(8, 3, "CLGE")
+for i in range(offset + 1):
+    scr.add_polygon(12, 5, 4, 1, u'\u2593')
+    scr.add_polygon(10, 5, 5, 1, u'\u2591')
+    scr.add_polygon(8, 3, 6, 2, u'\u2592')
 
+    scr.add_string(8, 3, "CLGE")
 
-scr.add_string(5, 7, "MADE")
-scr.add_string(11, 7, "WITH")
-scr.add_string(8, 8, "CLGE")
+    scr.add_string(5, 7 + offset - i, "MADE")
+    scr.add_string(11, 7 + offset - i, "WITH")
+    scr.add_string(8, 8 + offset - i, "CLGE")
 
-scr.render()
+    scr.render()
 
 time.sleep(5)
 
